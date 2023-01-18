5884b73c5342f9b169230999d21501c62eedcc05
NetoPaiva <netopaiva@yahoo.com>
2023-01-18 02:14:35 -0300

    Versão final do projeto. Edição e inclusão de arquivos; atualização do README. Entrega e publicação do projeto.

--- README.md
+++ README.md
@@ -1,15 +1,68 @@
-# t4-one_challenge
-## Repositório do Projeto Codificador/Decodificador de texto
+# codDoc.text(t4_one);
+## Projeto Codificador/Decodificador de texto
 
-### Challenge turma 4 - ONE (Oracle Next Education) 
+### Challenge turma 4 - ONE (Oracle Next Education)
+
 - Oferecimento: **Oracle**
 - Realização: **Alura**
 
-#### Desenvolver aplicação web para criptografar e descritopgrafar textos.
+#### Desenvolver aplicação web para codificar e decodificar textos
 
-1. Criar a estrutura HTML
-2. Definir a estilização CSS
-3. Programar as funcionalidades JS
-4. Armazenar as versões do desenvolvimento no repositório GitHub
-5. Publicar no GitHub Pages
-6. Formalizar a entrega na plataforma da Alura
+1. Criar repositório GitHub
+2. Iniciar versionamento
+3. Criar a estrutura HTML
+4. Programar funcionalidades JS
+5. Definir estilização CSS
+6. Publicar no GitHub Pages
+7. Finalizar versionamento
+8. Formalizar a entrega na plataforma da Alura
+
+#### Estrutura do repositório
+
+- Pasta "projeto" com os aqruivos html, css e js
+- Pasta "documentação" com os materiais pesquisados, registros do projeto e demais anotações
+
+---
+
+#### O desafio
+
+- Desenvolver página web com recursos para codificar/decodificar textos
+
+- Layout sugerido:
+	- cabeçalho com logo
+	- área de entrada de texto
+	- aviso de caracteres inválidos
+	- botões para codificar e decodificar
+	- imagem ilustrativa
+	- textos de avisos para o usuário
+	- área de saída de texto
+	- botão copiar
+
+- Funcionalidades exigidas
+
+	- botões codificar / decodificar
+		- retira texto da área de entrada
+		- oculta imagem ilustrativa
+		- mostra texto na área de saída
+		- mostra botão copiar
+
+	- botão copiar
+		- retira texto da área de saída
+		- guarda texto na memória
+
+- Layout e Funcionalidades acrescentadas
+	- título
+	- botões colar / recarregar
+	- rodapé com redes sociais e créditos
+
+	---
+
+	### E para celebrar mais esta consquista, música!
+
+	[![Nagô Reggae Instrumental - Rockers](https://img.youtube.com/vi/t_Hwx8nX65o/0.jpg)](https://youtu.be/t_Hwx8nX65o)
+
+	Aperte o play: [Nagô Reggae Instrumental - *Rockers*](https://youtu.be/t_Hwx8nX65o)
+
+	---
+
+	/*  ---. fim ;) */
--- projeto/netopaiva_script.js
+++ projeto/netopaiva_script.js
@@ -1,101 +1,111 @@
-//  script JS Neto Paiva - Projeto Codificar - Challenge t4 ONE/Alura jan-2023
+/*  script JS Neto Paiva
+		Projeto Codificar - Challenge t4 ONE/Alura jan-2023 */
 
 
-//  Funções auxiliares
-//  ==================
+/*  Funções auxiliares
+		================== */
 
-//	- buscar elemento HTML pelo ID:
-		let buscaId = b => document.getElementById(b);
+//	buscar elemento HTML pelo ID:
+let buscaId = b => document.getElementById(b);
 
-//	- limpar valor do ID
-		let limpaArea = (a,b) => buscaId(a).value = b;
+//	limpar valor do ID
+let limpaArea = (a,b) => buscaId(a).value = b;
 
-//	- copiar valor do elemento HTML
-		let copiaMemo = m => navigator.clipboard.writeText(m.value);
+//	copiar valor do elemento HTML
+let copiaMemo = m => navigator.clipboard.writeText(m.value);
 
-//	- colar texto no elemento HTML
-		let colaMemo = () => navigator.clipboard.readText();
+//	colar texto no elemento HTML
+let colaMemo = () => navigator.clipboard.readText();
 
-//	- detectar evento do usuário
-		let captaEvento = (x,y,z) => x.addEventListener(y,z);
+//	detectar evento do usuário
+let captaEvento = (x,y,z) => x.addEventListener(y,z);
 
-//	- recarregar a página:
-		let recarrega = x => location.reload(true);
-
-
-//	- Receber seletores HTML áreas de texto e botões
-//  ================================================
-
-		const textoEntrada = buscaId('entrada');
-		const textoSaida = buscaId('saida');
-		const btnCopiar = buscaId('copiar');
-		const btnColar = buscaId('colar');
-		const btnRecarregar = buscaId('recarregar');
-		const btnCodificar = buscaId('codifica');
-		const btnDecodificar = buscaId('decodifica');
+//	recarregar a página:
+let recarrega = x => location.reload(true);
 
 
-//  Funções dos botões
-//  ==================
+/*	Receber seletores HTML áreas de texto e botões
+		============================================== */
 
-//- ainda falta programar ocultar e mostrar textarea/imagem e botões
+const textoEntrada = buscaId('entrada');
+const textoSaida = buscaId('saida');
+const btnCopiar = buscaId('copiar');
+const btnColar = buscaId('colar');
+const btnRecarregar = buscaId('recarregar');
+const btnCodificar = buscaId('codifica');
+const btnDecodificar = buscaId('decodifica');
+
+
+/*  Funções Codificar / Decodificar
+		===============================*/
 
 //	Codificar
-		let codificar = captaEvento(btnCodificar, 'click', () => {
-			var doc = buscaId('entrada').value;
-
-			doc = doc
-				.normalize('NFD')
-				.replace(/[^a-zA-Z\s]/g, "")
-				.toLowerCase();
-
-	//		só nesta ordem codifica e decodifica corretamente
+let codificar = captaEvento(btnCodificar, 'click', () => {
+	// variável recebe o valor da área de entrada de texto
+	var doc = buscaId('entrada').value;
+	// verifica se área está vazia - emite alerta
+	if(doc == ''){
+		alert('Ops, não há texto para codificar!');
+	// realiza a função se não estiver vazia
+	} else{
+			doc = doc.normalize('NFD')
+							 .replace(/[^a-zA-Z\s]/g, "")
+							 .toLowerCase();
+	// só nesta ordem codifica e decodifica corretamente
+	// código alterado de "ai" para "ati" para evitar erro ao decodificar
 			doc = doc.replace(/e/g, 'enter')
-							  .replace(/i/g, 'imes')
-			          .replace(/a/g, 'ai')
-			          .replace(/o/g, 'ober')
-			          .replace(/u/g, 'ufat');
-
+							 .replace(/i/g, 'imes')
+							 .replace(/a/g, 'ati')
+							 .replace(/o/g, 'ober')
+							 .replace(/u/g, 'ufat');
+	// insere texto codificado da área de saída
 			buscaId('saida').value = doc;
+	// ação de ocultar a tela1 e mostrar a tela2
+			buscaId('tela1').style.display = 'none';
+			buscaId('tela2').style.display = 'block';
+	// ação para limpar a área de entrada de texto
 			limpaArea('entrada', '');
-		});
-
-		function copiar(){
-			var textoCop = document.getElementById('entrada');
-			textoCop.select();
-			document.execCommand('copy');
-			alert ('Texto Copiado');
 		}
+});
 
-//	Decodificar
-		captaEvento(btnDecodificar, 'click', () => {
-			var doc = buscaId('entrada').value;
-
-	//		só nesta ordem codifica e decodifica corretamente
-			doc = doc.replace(/enter/g, 'e');
-			doc = doc.replace(/imes/g, 'i');
-			doc = doc.replace(/ai/g, 'a');
-			doc = doc.replace(/ober/g, 'o');
-			doc = doc.replace(/ufat/g, 'u');
-
+// Decodificar
+captaEvento(btnDecodificar, 'click', () => {
+	var doc = buscaId('entrada').value;
+	if(doc == ''){
+		alert('Ops, não há texto para decodificar!');
+	} else{
+			doc = doc.replace(/enter/g, 'e')
+							 .replace(/imes/g, 'i')
+							 .replace(/ati/g, 'a')
+							 .replace(/ober/g, 'o')
+							 .replace(/ufat/g, 'u');
 			buscaId('saida').value = doc;
+			buscaId('tela1').style.display = 'none';
+			buscaId('tela2').style.display = 'block';
 			limpaArea('entrada', '');
-		});
+		}
+});
 
-//  Copiar
-		captaEvento(btnCopiar, 'click', () => {
-			copiaMemo(textoSaida);
-			limpaArea('saida', '');
-		});
 
-//  Colar
-		captaEvento(btnColar, 'click', async () => {
-			const colar = await colaMemo();
-			textoEntrada.value = colar;
-		});
+/*	Funções copiar / colar / recarregar
+		=================================== */
 
-//  Recarregar a página
-		captaEvento(btnRecarregar, 'click', recarrega);
+// Copiar
+captaEvento(btnCopiar, 'click', () => {
+	copiaMemo(textoSaida);
+	limpaArea('saida', '');
+});
+
+// Colar
+captaEvento(btnColar, 'click', async () => {
+	const colar = await colaMemo();
+	textoEntrada.value = colar;
+	buscaId('tela2').style.display = 'none';
+	buscaId('tela1').style.display = 'block';
+});
+
+// Recarregar a página
+captaEvento(btnRecarregar, 'click', recarrega);
 
 
 //---  .fim :)
